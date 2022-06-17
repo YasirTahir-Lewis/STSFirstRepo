d81510f5b65541272274c01519f8bbe3c4d61642
Yasir Tahir <yasirtahir1999@gmail.com>
2022-06-17 12:07:19 -0500

    Initial commit

---
+++ SpringbootProfiles/src/main/java/com/apolis/entity/Feature.java
@@ -0,0 +1,10 @@
+package com.apolis.entity;
+
+public class Feature {
+
+	public static void main(String[] args) {
+		System.out.println("Feature branch");
+
+	}
+
+}
--- SpringbootProfiles/src/main/java/com/apolis/springboot/SpringbootProfilesApplication.java
+++ SpringbootProfiles/src/main/java/com/apolis/springboot/SpringbootProfilesApplication.java
@@ -13,7 +13,7 @@ public class SpringbootProfilesApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(SpringbootProfilesApplication.class, args);
-		User user = (User)context.getBean("dbData");
+		User user = (User) context.getBean("dbData");
 		System.out.println(user);
 	}
 
